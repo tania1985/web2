2a646f2dbc60a4a1dd62e01bbd3cec9899cd3e95
tania1985 <testevezbautista@danielcastelao.org>
2024-12-26 19:40:20 +0100

    actualizando

--- js/registro.js
+++ js/registro.js
@@ -1,6 +1,7 @@
 var inputPass=document.getElementById("password");
 var inputRePass=document.getElementById("confirm-password");
 var btnCrear=document.getElementById("btnCrear");
+var msg=document.getElementById("msg");
 
 inputPass.oninput=validacionInput;
 inputRePass.oninput=validacionInput; 
@@ -13,8 +14,10 @@ function validacionInput(){
     var txtRePass=inputRePass.value;
     if(txtPass==txtRePass){
         btnCrear.disabled=false;
+        msg.style.display="none";
     }else{
         btnCrear.disabled=true;
+        msg.style.display="block";
     }
     //console.log(txtPass);
 }
