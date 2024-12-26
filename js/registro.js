78c790c7b36acd435d4c5a1a83bde050e95457fd
tania1985 <testevezbautista@danielcastelao.org>
2024-12-26 18:20:18 +0100

    creando un js

---
+++ js/registro.js
@@ -0,0 +1,8 @@
+var inputPass=document.getElementById("password");
+var inputPass=document.getElementById("confirm-password");
+var btnCrear=document.getElementById("btnCrear");
+
+inputPass.onchange=function(){
+    var txtPass=inputPass.value;
+    console.log(txtPass);
+}
