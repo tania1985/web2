44c170e9f309e914386ecfce1ecfc11bb23ca4d0
tania1985 <testevezbautista@danielcastelao.org>
2024-12-27 18:38:44 +0100

    creando main

--- js/login.js
+++ js/login.js
@@ -5,5 +5,7 @@ formulario.onsubmit =  function(e){
     var password = document.getElementById("password").value;
     if(usuario == "admin" && password == "admin"){
         window.location.href = "main.html";
-}
+    }else{
+        alert("Usuario o contraseña incorrectos");
+    }
 }
