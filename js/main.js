686130a74e1c7fba1692a730e32f51b8317f4b4c
tania1985 <testevezbautista@danielcastelao.org>
2025-01-02 16:29:02 +0100

    creando javascript

--- js/main.js
+++ js/main.js
@@ -1,16 +1,2 @@
- // Obtener la imagen del usuario y el menú
- const userImage = document.getElementById("userImage");
- const userMenu = document.getElementById("userMenu");
-
- // Agregar evento de clic en la imagen
- userImage.addEventListener("click", function() {
-     // Mostrar un mensaje de alerta
-     alert("¡Has hecho clic en la foto del usuario!");
-
-     // Cambiar el estilo de display para ocultar el menú
-     if (userMenu.style.display === "none") {
-         userMenu.style.display = "block"; // Mostrar el menú si ya estaba oculto
-     } else {
-         userMenu.style.display = "none"; // Ocultar el menú
-     }
- });
+var user=document.getElementById("imgUser");
+user.addEventListener("click", function(){alert("Hola")});
