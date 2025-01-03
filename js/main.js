4cf75a153075af357cfe677de8484490e924a366
tania1985 <testevezbautista@danielcastelao.org>
2025-01-03 17:47:52 +0100

    creando nuevo main

--- js/main.js
+++ js/main.js
@@ -41,3 +41,34 @@ window.onload = function() {
     var fechaHoy = new Date().toISOString().split('T')[0]; // Obtener la fecha en formato YYYY-MM-DD
     document.getElementById('fecha').value = fechaHoy; // Asignar la fecha al campo de fecha
 };
+
+var formulario = document.getElementById('formIncidencias');
+formulario.onsubmit = function(e) {
+    e.preventDefault(); // Evitar que el formulario se envíe
+    var incidencia =document.getElementById('descripcion').value;
+    var fecha =document.getElementById('fecha').value;
+    var tabla =document.getElementById('tablaIncidencias');
+    var tr = document.createElement('tr');
+    var tr=document.createElement('tr');
+    var td1=document.createElement('td');
+    var td2=document.createElement('td');
+    var td3=document.createElement('td');
+    var td4=document.createElement('td');
+    var i=document.createElement('i');
+    i.classList.add('fa-solid');
+    i.classList.add('fa-trash');
+    i.onclick=function(e){
+        var row=this.closest('tr');
+        row.remove();
+    };
+    td1.innerHTML=99;
+    td2.innerHTML=fecha;
+    td3.innerHTML=incidencia;
+    td4.appendChild(i);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    tabla.appendChild(tr);
+    formulario.reset();
+};
